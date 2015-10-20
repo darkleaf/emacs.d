433070e50b74e9feeb680b5ae8bbcad8681f0cf9
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2015-10-20 12:40:09 +0300

    fix

--- init.el
+++ init.el
@@ -2,8 +2,19 @@
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
-;; global
-(setq make-backup-files nil)
+;; Disable GUI components
+(tooltip-mode      -1)
+(menu-bar-mode     -1) ;; отключаем графическое меню
+(tool-bar-mode     -1) ;; отключаем tool-bar
+(scroll-bar-mode   -1) ;; отключаем полосу прокрутки
+(blink-cursor-mode -1) ;; курсор не мигает
+(setq use-dialog-box     nil) ;; никаких графических диалогов и окон - все через минибуфер
+(setq redisplay-dont-pause t)  ;; лучшая отрисовка буфера
+(setq ring-bell-function 'ignore) ;; отключить звуковой сигнал
+
+;; Disable backup/autosave files
+(setq make-backup-files        nil)
+(setq auto-save-default        nil)
 
 ;; escape fix
 ;; https://github.com/chrisdone/god-mode/issues/43#issuecomment-67193877
