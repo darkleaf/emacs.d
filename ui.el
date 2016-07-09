0c4a39727f5fd3d9b8452f23946e8916a1e425a9
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-07-09 23:26:33 +0400

    change font

--- ui.el
+++ ui.el
@@ -6,4 +6,5 @@
 (tool-bar-mode -1)
 
 (when (window-system)
-  (set-face-attribute 'default nil :family "Source Code Pro" :height 165))
+  ;;(set-face-attribute 'default nil :family "Source Code Pro" :height 165)
+  (set-face-attribute 'default nil :family "Courier" :height 165))
