8e41078994d460b86cae5089a9f648d540bb40fc
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-07-12 20:28:54 +0400

    add smartparens

--- Cask
+++ Cask
@@ -6,7 +6,9 @@
 (depends-on "helm-projectile"
 	    :git "https://github.com/bbatsov/helm-projectile.git"
 	    :ref "f41141a")
+
 (depends-on "nlinum")
+(depends-on "smartparens")
 
 ;;clojure
 (depends-on "clojure-mode")
--- prog-mode.el
+++ prog-mode.el
@@ -2,3 +2,8 @@
 (setq nlinum-format "%d ")
 (add-hook 'prog-mode-hook 'nlinum-mode)
 
+;;smartparens
+(customize-set-value 'sp-base-key-bindings 'sp)
+(require 'smartparens-config)
+(add-hook 'prog-mode-hook 'smartparens-strict-mode)
+(add-hook 'prog-mode-hook 'show-smartparens-mode)
