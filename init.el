342498dd63f8e257d37e7fef924a00f6c52fa12d
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2017-04-23 12:49:48 +0400

    save

--- init.el
+++ init.el
@@ -141,5 +141,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (exec-path-from-shell dockerfile-mode ibuffer-vc ibuffer-projectile dim spaceline dashboard magit yaml-mode parinfer web-mode clojure-mode counsel-projectile ivy projectile nlinum use-package))))
-(custom-set-faces)
+    (exec-path-from-shell dim dashboard magit parinfer dockerfile-mode yaml-mode web-mode clj-refactor cider clojure-mode counsel-projectile ivy projectile nlinum ibuffer-vc use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
