daaa0accf1e3c8cea09571bca8279238d8125e3d
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2020-03-17 22:25:28 +0400

    save

--- ext/my-fix.el
+++ ext/my-fix.el
@@ -1,7 +1,12 @@
+(if (string-equal "darwin" (symbol-name system-type))
+    ;;(setenv "LANG" "en_US.UTF-8"))
+    (setenv "LANG" "ru_RU.UTF-8"))
+
 (use-package exec-path-from-shell
   :if window-system
   :config
   (add-hook 'eshell-mode-hook 'exec-path-from-shell-initialize)
   (exec-path-from-shell-initialize))
+
 
 (provide 'my-fix)
--- ext/my-lisp.el
+++ ext/my-lisp.el
@@ -1,14 +1,32 @@
+;; https://github.com/danieroux/emacs/blob/master/mine/djr-clojure.el#L6
+;; The `clojure-indent-function` has hardcoded 'always-align statements in it
+;; This specifically messes with (:require) statements
+;; (defun djr-tonsky-indent (indent-point state)
+;;   (goto-char (elt state 1))
+;;   (if (clojure--not-function-form-p)
+;;       (1+ (current-column))
+;;     (forward-char 1)
+;;     (clojure--normal-indent calculate-lisp-indent-last-sexp 'always-indent)))
+
 (use-package clojure-mode
   :config
   (put-clojure-indent 'clojure.spec/fdef 1)
   (put-clojure-indent 'clojure.spec.alpha/fdef 1)
   (put-clojure-indent 'clojure.core.match/match 1)
   (put-clojure-indent 'not-join 1))
+  ;; :init
+  ;; (progn
+  ;;   (add-hook 'clojure-mode-hook
+  ;;             (lambda ()
+  ;;               ;; Enable https://tonsky.me/blog/clojurefmt
+  ;;               (setq clojure-indent-style 'always-indent)
+  ;;               (setq clojure-align-forms-automatically t)
+  ;;               (setq-local lisp-indent-function #'djr-tonsky-indent)))))
 
 (use-package cider
   :init
-  (setq cider-prompt-save-file-on-load 'always-save))
-  ;;(setq cider-clojure-cli-global-options "-Adev"))
+  (setq cider-prompt-save-file-on-load 'always-save)
+  (setq cider-clojure-cli-global-options "-Adev"))
 
 (use-package clj-refactor
   :config
